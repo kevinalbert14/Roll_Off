b62f3dd73c8fe8d78783f2c2aabe982352389cef
Kevin A <W0774192@myscc.ca>
2022-07-04 21:07:00 -0400

    added multiple new functions

--- main.py
+++ main.py
@@ -16,77 +16,102 @@ from conf_matrix import conf_matrix
 from classifier_state import classifier_state
 from tsreport import events, tsreport
 from continuous_data import continuous, evelogs, samp_pred
+from single_events import single_events
+from keras.models import load_model
+
+def choice():
+    sel = 0
+    while sel not in [1,2]:
+        sel = int(input("Press 1 to train the model and 2 to run the prediction"))
+    return sel
+
+selection = choice()
+
+while selection == 1:
 
 
-target_dict = dict([("Dump", 0),("Loaded",1),("Unloaded",2),("Inter",3)])
-
-images, labels = image_load()
-
-labels_int = [] # use the target_dict to convert the string labels to an array of integers
-for i in labels:
-      n_key = target_dict[i]
-      labels_int.append(n_key)
-labels_int = np.array(labels_int)
-
-X_train, Y_train, X_test, Y_test, X_val, Y_val = splitting_train_test(images, labels_int)
-
-model = mod()
-print(model.summary())
-model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
+      if selection == 1:
 
 
-history = model.fit(X_train, Y_train, epochs=8,
-                    validation_data=(X_val, Y_val))
+            target_dict = dict([("Dump", 0),("Loaded",1),("Unloaded",2),("Inter",3)])
 
-test_loss, test_acc = model.evaluate(X_test,  Y_test, verbose=2)
+            images, labels = image_load()
 
-accuracy = history.history["accuracy"]
-val_accuracy = history.history["val_accuracy"]
+            labels_int = [] # use the target_dict to convert the string labels to an array of integers
+            for i in labels:
+                  n_key = target_dict[i]
+                  labels_int.append(n_key)
+            labels_int = np.array(labels_int)
 
-accuracy_graph(accuracy, val_accuracy)
+            X_train, Y_train, X_test, Y_test, X_val, Y_val = splitting_train_test(images, labels_int)
 
-test_prend = model.predict(X_test)
-
-print(conf_matrix(Y_test, test_prend))
-
-testing_file = input("Enter the destiation of the testing file")
-
-test_img, ip_im_name, speed_data = test_loader(testing_file)
-
-ip_pred = model.predict(test_img)
-
-Y = timestamps(ip_pred, ip_im_name, speed_data)
+            model = mod()
+            print(model.summary())
+            model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
 
 
-### This part of the code needs to be reviewed
-#Converting the predicted Numbers into their corresponding states "Loaded" , "Unloading" , "Inter" and "Dump"
-ip_pred_2 = [] # use the target_dict to convert the string labels to an array of integers\n",
-target_dict2 = dict([(0,'Dump'),(1,'Loaded'),(2,'Unloaded'),(3,'Inter')])
-for i in Y['Pred']:
-      need = target_dict2[i]
-      ip_pred_2.append(need)
+            history = model.fit(X_train, Y_train, epochs=8, validation_data=(X_val, Y_val))
+
+            test_loss, test_acc = model.evaluate(X_test,  Y_test, verbose=2)
+
+            accuracy = history.history["accuracy"]
+            val_accuracy = history.history["val_accuracy"]
+
+            accuracy_graph(accuracy, val_accuracy)
+
+            test_prend = model.predict(X_test)
+
+            print(conf_matrix(Y_test, test_prend))
+
+            model.save('model_weight_cnn_86.h5')
+
+            print(" Model has be trained and saved with the new data and the ")
+
+            selection = 2
+
+else:
+
+      model = load_model('model_weight_cnn_86.h5')
+
+      testing_file = input("Enter the destiation of the testing file")
+
+      test_img, ip_im_name, speed_data = test_loader(testing_file)
+
+      ip_pred = model.predict(test_img)
+
+      Y = timestamps(ip_pred, ip_im_name, speed_data)
+
+
+      ### This part of the code needs to be reviewed
+      #Converting the predicted Numbers into their corresponding states "Loaded" , "Unloading" , "Inter" and "Dump"
+      ip_pred_2 = [] # use the target_dict to convert the string labels to an array of integers\n",
+      target_dict2 = dict([(0,'Dump'),(1,'Loaded'),(2,'Unloaded'),(3,'Inter')])
+      for i in Y['Pred']:
+            need = target_dict2[i]
+            ip_pred_2.append(need)
 
 
 
-ip_pred_2, number, target_dict2 = classifier_state(ip_pred_2, Y)
+      ip_pred_2, number, target_dict2 = classifier_state(ip_pred_2, Y)
 
-print( target_dict2)
+      print( target_dict2)
 
 
-report_1 = rep_1(ip_pred_2,Y['TS']) ## This chart needs to be shown in the report.
+      report_1 = rep_1(ip_pred_2,Y['TS']) ## This chart needs to be shown in the report.
 
-start_time, end_time, time_elapsed = tsreport(Y["Pred"], Y["TS"], target_dict2)
+      start_time, end_time, time_elapsed = tsreport(Y["Pred"], Y["TS"], target_dict2)
 
-Lding, Uding, Lded, Ulded, Ding = events(ip_pred_2, Y["TS"])
+      Lding, Uding, Lded, Ulded, Ding = events(ip_pred_2, Y["TS"])
 
-Uding_log, Lding_log, Ding_log = continuous(ip_pred_2,Y['TS'],Lding, Uding, Ding)
+      Uding_log, Lding_log, Ding_log = continuous(ip_pred_2,Y['TS'],Lding, Uding, Ding)
 
-events_log = evelogs(Uding_log, Lding_log, Ding_log)
+      events_log = evelogs(Uding_log, Lding_log, Ding_log)
 
-event_predictions = samp_pred(events_log, ip_pred_2, Y)
+      event_predictions = samp_pred(events_log, ip_pred_2, Y)
 
-individual_events (events_log, Y, ip_pred_2, event_predictions)
+      individual_events (events_log, Y, ip_pred_2, event_predictions)
+
+      single_events(Lding, Uding, Lded, Ulded, Ding)
 
 
 
